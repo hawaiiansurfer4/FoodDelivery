4b2d3e2d2af65e1fd8f6153620e9f100e2420786
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-16 12:47:22 -0400

    Adds beginning code for collection view

--- FoodDelivery/ContentView.swift
+++ FoodDelivery/ContentView.swift
@@ -7,6 +7,39 @@
 
 import SwiftUI
 
+class FoodController: UICollectionViewController {
+    
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        super.init(collectionViewLayout: layout)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    private let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+        navigationItem.title = "Food Delivery"
+        
+        collectionView.register(UICollectionView.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
@@ -16,6 +49,18 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Container().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct Container: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> UIViewController {
+            UINavigationController(rootViewController: FoodController())
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+        
+        typealias UIViewControllerType = UIViewController
     }
 }
