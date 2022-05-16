7349b1b95d556cfedea2008b9c8a52e43d773530
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-16 14:40:06 -0400

    Adds comp layout

--- FoodDelivery/ContentView.swift
+++ FoodDelivery/ContentView.swift
@@ -8,59 +8,66 @@
 import SwiftUI
 
 class FoodController: UICollectionViewController {
-    
+
     init() {
-        let layout = UICollectionViewFlowLayout()
+        let layout = UICollectionViewCompositionalLayout { sectionNumber, env in
+            let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(300)), subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+
+            return section
+        }
+
         super.init(collectionViewLayout: layout)
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
+        return 5
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
         cell.backgroundColor = .red
         return cell
     }
-    
+
     private let cellId = "cellId"
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         collectionView.backgroundColor = .white
         navigationItem.title = "Food Delivery"
-        
-        collectionView.register(UICollectionView.self, forCellWithReuseIdentifier: cellId)
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Container().edgesIgnoringSafeArea(.all)
+    }
+
+    struct Container: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> UIViewController {
+            UINavigationController(rootViewController: FoodController())
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+
+        }
+
+        typealias UIViewControllerType = UIViewController
     }
 }
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Container().edgesIgnoringSafeArea(.all)
-    }
-    
-    struct Container: UIViewControllerRepresentable {
-        func makeUIViewController(context: Context) -> UIViewController {
-            UINavigationController(rootViewController: FoodController())
-        }
-        
-        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-            
-        }
-        
-        typealias UIViewControllerType = UIViewController
     }
 }
